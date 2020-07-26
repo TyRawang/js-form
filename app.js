6d417ba8d8019e42ab0cd02116365d0bb6af4ff2
Vishal Sahu <vvishal.online@gmail.com>
2020-07-26 10:47:06 +0530

    Implemented validations Next and Previous button functionality

--- app.js
+++ app.js
@@ -22,6 +22,101 @@ function showTab(n) {
   }
 
 }
+function prev(){
+	if(currentTab>0){
+		for(var i=0;i<3;i++){		
+			if(i==currentTab-1){
+				$(".tab"+i).show();			
+			}else{
+				$(".tab"+i).hide();						
+			}
+		}
+		currentTab--;
+		if(currentTab==2){
+			$("#nextBtn").hide();
+			$("#submitBtn").show();
+		}else{
+			$("#nextBtn").show();
+			$("#submitBtn").hide();
+		}		
+	}
+}
+
+
+function next(){
+	if(currentTab < 2){
+		if(validate()){
+			for(var i=0;i<3;i++){		
+				if(i==currentTab+1){
+					$(".tab"+i).show();			
+				}else{
+					$(".tab"+i).hide();						
+				}
+			}
+			currentTab++;
+			if(currentTab==2){
+				$("#nextBtn").hide();
+				$("#submitBtn").show();
+			}else{
+				$("#nextBtn").show();
+				$("#submitBtn").hide();
+			}		
+		}
+	}
+}
+
+function validate(){
+	switch(currentTab){
+		case 0:
+			return validateTab0();
+			break;
+		case 1:
+			return validateTab1();
+			break;
+		case 2:
+			return validateTab2();
+			break;
+	}
+	return true;
+}
+
+function submitAction(){
+	if(validate()){
+		// TO BE IMPLEMENTED
+	}
+}
+
+function validateTab0(){
+	if($("input[name='time']:checked").val()==undefined){
+		$("#errorFrequency").show();
+		return false;
+	}else{
+		$("#errorFrequency").hide();
+		return true;		
+	}
+}
+
+
+function validateTab1(){
+	if($("#houseType").val()==''){
+		$("#errorHouseType1").show();
+		return false;
+	}else{
+		$("#errorHouseType1").hide();
+		return true;		
+	}
+}
+
+function validateTab2(){
+	if($("#houseType2").val()==''){
+		$("#errorHouseType2").show();
+		return false;
+	}else{
+		$("#errorHouseType2").hide();
+		return true;		
+	}
+}
+
 
 function nextPrev(n) {
   var x = document.getElementsByClassName("tab"); 
