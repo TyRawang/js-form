5e009014477859005b259afb210ccefb95b0ff81
Vishal Sahu <vvishal.online@gmail.com>
2020-07-26 16:15:25 +0530

    Removed some unused javascript functions

--- app.js
+++ app.js
@@ -1,27 +1,5 @@
 var currentTab = 0;
-showTab(currentTab);
 
-function showTab(n) {
-  var x = document.getElementsByClassName("tab");
-  x[n].style.display = "block";
-
-  // console.log(x[n]);
-  // console.log(n);
-
-  if (n==0) {
-    document.getElementById("prevBtn").style.display = "none";
-
-  } else {
-    document.getElementById("prevBtn").style.display = "inline";
-  }
-
-  if (n==(x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Submit"
-  } else {
-    document.getElementById("nextBtn").innerHTML = "Next"; 
-  }
-
-}
 function prev(){
 	if(currentTab>0){
 		for(var i=0;i<3;i++){		
@@ -132,23 +110,6 @@ function validateTab2(){
 	}
 }
 
-
-function nextPrev(n) {
-  var x = document.getElementsByClassName("tab"); 
-
-  console.log("v: "+!validateForm)
-  if (n==1 && !validateForm()) return false;
-
-  x[currentTab].style.display ="none"; 
-  currentTab = currentTab + 1;
-  if(currentTab >=x.length) {
-    document.getLelementBy("regForm").submit();
-    return false; 
-  }
-
-  showTab(currentTab); 
-
-}
 
 function validateForm() {
   var x, y, i, valid = true; 
